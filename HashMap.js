291b7f7479d0bf43c4f5f7116a138e88a7d0ffa1
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-10-25 23:40:00 +0900

    Add LinkedList.js

--- HashMap.js
+++ HashMap.js
@@ -1,27 +1,19 @@
+import LinkedList from "./LinkedList";
+
 export default class HashMap {
   #capacity = 16;
+  #buckets = Array.from({ length: this.#capacity }, () => null);
   #loadFactor = 0.75;
   #length = 0;
 
-  #hash(key) {
-    let hashCode = 0;
-
-    const primeNumber = 31;
-    for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
-    }
-
-    return hashCode;
-  }
-
   set(key, value) {
-
+    const hashcode = this.#hash(key);
   }
-
+  
   get(key) {
-
+    
   }
-
+  
   has(key) {
 
   }
@@ -29,24 +21,35 @@ export default class HashMap {
   remove(key) {
 
   }
-
+  
   length() {
-
+    
   }
-
+  
   clear() {
-
+    
   }
-
+  
   keys() {
-
+    
   }
-
+  
   values() {
-
+    
   }
-
+  
   entries() {
     
+  }
+  
+  #hash(key) {
+    let hashCode = 0;
+  
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
+  
+    return hashCode;
   }
 }
