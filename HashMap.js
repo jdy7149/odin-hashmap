5c486cfe891b7645003ae99eac276ffea69f0ebb
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-10-25 22:30:34 +0900

    End boilerplate

---
+++ HashMap.js
@@ -0,0 +1,52 @@
+export default class HashMap {
+  #capacity = 16;
+  #loadFactor = 0.75;
+  #length = 0;
+
+  #hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {
+
+  }
+
+  get(key) {
+
+  }
+
+  has(key) {
+
+  }
+
+  remove(key) {
+
+  }
+
+  length() {
+
+  }
+
+  clear() {
+
+  }
+
+  keys() {
+
+  }
+
+  values() {
+
+  }
+
+  entries() {
+    
+  }
+}
