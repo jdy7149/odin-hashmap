b9b94c9d41d1c7045339a32647da7e72a7640162
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-10-26 20:13:03 +0900

    End implementation

--- HashMap.js
+++ HashMap.js
@@ -5,19 +5,52 @@ export default class HashMap {
   #length = 0;
 
   set(key, value) {
+    const hashCode = this.#hash(key);
 
+    const bucket = this.#buckets[hashCode];
+
+    const index = bucket.findIndex((node) => node.key === key);
+
+    if (index === -1) {
+      bucket.push({ key, value });
+      this.#length++;
+    } else {
+      bucket[index].value = value;
+    }
+
+    if (this.#isExceedLoad()) this.#growBuckets();
   }
   
   get(key) {
+    const hashCode = this.#hash(key);
 
+    const bucket = this.#buckets[hashCode];
+
+    return bucket.find((node) => node.key === key)?.value ?? null;
   }
   
   has(key) {
+    const hashCode = this.#hash(key);
 
+    const bucket = this.#buckets[hashCode];
+
+    return bucket.some((node) => node.key === key);
   }
 
   remove(key) {
+    const hashCode = this.#hash(key);
 
+    const bucket = this.#buckets[hashCode];
+
+    const index = bucket.findIndex((node) => node.key === key);
+
+    if (index !== -1) {
+      bucket.splice(index, 1);
+      this.#length--;
+      return true;
+    }
+
+    return false;
   }
   
   length() {
@@ -25,19 +58,35 @@ export default class HashMap {
   }
   
   clear() {
+    this.#buckets.forEach((bucket) => {
+      bucket.length = 0;
+    });
 
+    this.#length = 0;
   }
   
   keys() {
+    return this.#buckets.reduce((result, bucket) => {
+      bucket.forEach((node) => result.push(node.key));
 
+      return result;
+    }, []);
   }
   
   values() {
+    return this.#buckets.reduce((result, bucket) => {
+      bucket.forEach((node) => result.push(node.value));
 
+      return result;
+    }, []);
   }
   
   entries() {
+    return this.#buckets.reduce((result, bucket) => {
+      bucket.forEach((node) => result.push([node.key, node.value]));
 
+      return result;
+    }, []);
   }
   
   #hash(key) {
@@ -52,7 +101,16 @@ export default class HashMap {
   }
 
   #growBuckets() {
+    this.#capacity *= 2;
 
+    const prevBuckets = this.#buckets;
+
+    this.#buckets = Array.from({ length: this.#capacity }, () => []);
+    this.#length = 0;
+
+    prevBuckets.forEach((bucket) => {
+      bucket.forEach(({ key, value }) => this.set(key, value));
+    });
   }
 
   #isExceedLoad() {
